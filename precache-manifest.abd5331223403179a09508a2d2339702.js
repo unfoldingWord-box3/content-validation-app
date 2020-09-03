c5d8ea47e5d749d8ba6cdd15d36a199bbb776d44
mandolyte <cecil.new@gmail.com>
2020-09-03 16:13:48 -0400

    Updates

---
+++ precache-manifest.abd5331223403179a09508a2d2339702.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af148bf25a732bf4b991740ab3ba7312",
+    "url": "/content-validation-app/index.html"
+  },
+  {
+    "revision": "63b7e3a371177024c2ef",
+    "url": "/content-validation-app/static/css/main.2f6ca397.chunk.css"
+  },
+  {
+    "revision": "e096aa66dc18acadd654",
+    "url": "/content-validation-app/static/js/2.62c2c5a9.chunk.js"
+  },
+  {
+    "revision": "a7bbfd45e0f4000fe30817fa6dfbbda0",
+    "url": "/content-validation-app/static/js/2.62c2c5a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63b7e3a371177024c2ef",
+    "url": "/content-validation-app/static/js/main.338569f8.chunk.js"
+  },
+  {
+    "revision": "49c500ab82845f4085ad",
+    "url": "/content-validation-app/static/js/runtime-main.0b520c22.js"
+  }
+]);
